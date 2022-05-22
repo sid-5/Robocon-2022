7f0d7974d86e12ebb3106ba671189e03c037685e
Atharva Puranik <puranik.atharva36@gmail.com>
2022-05-22 12:54:54 +0530

    Multi Ip R2 code

--- PS4_controller/R2_Code/R2_Code.ino
+++ PS4_controller/R2_Code/R2_Code.ino
@@ -1,6 +1,23 @@
 #include <PS4Controller.h>
 int prev_time = 0;
 
+int loco_dir1 = 5;
+int loco_pwm1 = 16; //RX2
+int loco_dir2 = 18;
+int loco_pwm2 = 17; //TX2
+ 
+int lagori_grab_dir = 33;
+int lagori_grab_pwm = 32;
+
+int lagori_lift_dir = 26;
+int lagori_lift_pwm = 25;
+
+int ball_pass_dir = 2;
+int ball_pass_pwm = 4;
+
+int ball_pick_dir = 17;
+int ball_pick_pwm = 16;
+
 void setup() {
   // put your setup code here, to run once:
   // PS4.begin("30:94:35:32:df:3e");
@@ -10,26 +27,26 @@ void setup() {
   
   ledcSetup(0, 5000, 8); //loco1 
   ledcSetup(1, 5000, 8); //loco2
-  ledcAttachPin(16, 0); //RX2
-  ledcAttachPin(17, 1); //TX2
-  pinMode(5, OUTPUT); //loco1_dir D5
-  pinMode(18, OUTPUT); // loco2_dir D18
+  ledcAttachPin(loco_pwm1, 0); //RX2
+  ledcAttachPin(loco_pwm2, 1); //TX2
+  pinMode(loco_dir1, OUTPUT); //loco1_dir D5
+  pinMode(loco_dir2, OUTPUT); // loco2_dir D18
 
   ledcSetup(2, 5000, 8); // Lagori Grab
   ledcSetup(3, 5000, 8); // Lagori Lift
 
-  ledcAttachPin(32, 2); // D32 -> Lagori Grab
-  ledcAttachPin(25, 3); // D25 -> Lagori Lift
-  pinMode(33, OUTPUT); // D33 -> Lagori Grab Dir
-  pinMode(26, OUTPUT); // D26 -> Lagori Lift Dir
+  ledcAttachPin(lagori_grab_pwm, 2); // D32 -> Lagori Grab
+  ledcAttachPin(lagori_lift_pwm, 3); // D25 -> Lagori Lift
+  pinMode(lagori_grab_dir, OUTPUT); // D33 -> Lagori Grab Dir
+  pinMode(lagori_lift_dir, OUTPUT); // D26 -> Lagori Lift Dir
 
   ledcSetup(4, 5000, 8); // Ball Pass
   ledcSetup(5, 5000, 8); // Ball Pick
 
-  ledcAttachPin(4, 4); // D4 -> Ball Pick
-  ledcAttachPin(16, 5); // D16 -> Ball Pass
-  pinMode(2, OUTPUT); // Ball Pick Dir
-  pinMode(17, OUTPUT); // Ball Pass Dir
+  ledcAttachPin(ball_pass_pwm, 4); // D4 -> Ball Pick
+  ledcAttachPin(ball_pick_pwm, 5); // D16 -> Ball Pass
+  pinMode(ball_pick_dir, OUTPUT); // Ball Pick Dir
+  pinMode(ball_pass_dir, OUTPUT); // Ball Pass Dir
 
   pinMode(13, INPUT); // Top Limit Switch
   pinMode(12, INPUT); // Bottom Limit Switch
@@ -42,29 +59,29 @@ void loop() {
       if (PS4.Up()){
         ledcWrite(0, 160);
         ledcWrite(1, 160);
-        digitalWrite(5, HIGH);
-        digitalWrite(18, LOW);      
+        digitalWrite(loco_dir1, HIGH);
+        digitalWrite(loco_dir2, LOW);      
       }
       
       else if (PS4.Down()){
         ledcWrite(0, 160);
         ledcWrite(1, 160);
-        digitalWrite(5, LOW);
-        digitalWrite(18, HIGH);  
+        digitalWrite(loco_dir1, LOW);
+        digitalWrite(loco_dir2, HIGH);  
       }
       
       else if (PS4.Left()){
         ledcWrite(0, 160);
         ledcWrite(1, 160);
-        digitalWrite(5, HIGH);
-        digitalWrite(18, HIGH);  
+        digitalWrite(loco_dir1, HIGH);
+        digitalWrite(loco_dir2, HIGH);  
       }
       
       else if (PS4.Right()){
         ledcWrite(0, 160);
         ledcWrite(1, 160);
-        digitalWrite(5, LOW);
-        digitalWrite(18, LOW);  
+        digitalWrite(loco_dir1, LOW);
+        digitalWrite(loco_dir2, LOW);  
       }
   
       else{
@@ -82,42 +99,42 @@ void loop() {
       if (PS4.Square()){
           Serial.println("Square");
           ledcWrite(2, 160);
-          digitalWrite(33, HIGH);
+          digitalWrite(lagori_grab_dir, HIGH);
       }
       else if ((PS4.Triangle()) && (!digitalRead(13))){
         Serial.println("Triangle");
         ledcWrite(3, 160);
-        digitalWrite(26, HIGH);
+        digitalWrite(lagori_lift_dir, HIGH);
       }
       else if (PS4.Circle()){
         Serial.println("Circle");
         ledcWrite(2, 160);
-        digitalWrite(33, HIGH);
+        digitalWrite(lagori_grab_dir, HIGH);
       }
       else if ((PS4.Cross()) && (!digitalRead(12))){
         Serial.println("Cross");
         ledcWrite(3, 160);
-        digitalWrite(26, LOW);
+        digitalWrite(lagori_lift_dir, LOW);
       }
       else if (PS4.L1()){
         Serial.println("L1");
         ledcWrite(4, 160);
-        digitalWrite(2, LOW);
+        digitalWrite(ball_pass_dir, LOW);
       }
       else if (PS4.L2()){
         Serial.println("L2");
         ledcWrite(4, 160);
-        digitalWrite(2, HIGH);
+        digitalWrite(ball_pass_dir, HIGH);
       }
       else if (PS4.R1()){
         Serial.println("R1");
         ledcWrite(5, 160);
-        digitalWrite(17, LOW);
+        digitalWrite(ball_pick_dir, LOW);
       }
       else if (PS4.R2()){
         Serial.println("R2");
         ledcWrite(5, 160);
-        digitalWrite(17, HIGH);
+        digitalWrite(ball_pick_dir, HIGH);
       }
       else{
         ledcWrite(2, 0);
