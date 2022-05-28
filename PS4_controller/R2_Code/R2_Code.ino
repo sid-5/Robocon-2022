3eb34689fd9753f7ee529e1b163c3a7f555f9b5b
Atharva Puranik <puranik.atharva36@gmail.com>
2022-05-28 23:27:11 +0530

    Fade effect for grab

--- PS4_controller/R2_Code/R2_Code.ino
+++ PS4_controller/R2_Code/R2_Code.ino
@@ -6,23 +6,26 @@ int loco_pwm1 = 16; //RX2
 int loco_dir2 = 18;
 int loco_pwm2 = 17; //TX2
  
-int lagori_grab_dir = 33;
-int lagori_grab_pwm = 32;
+int lagori_grab_dir = 22;
+int lagori_grab_pwm = 19;
 
-int lagori_lift_dir = 26;
-int lagori_lift_pwm = 25;
+int lagori_lift_dir = 23;
+int lagori_lift_pwm = 21;
 
-int ball_pass_dir = 2;
-int ball_pass_pwm = 4;
+int ball_pass_dir = 26;
+int ball_pass_pwm = 27;
 
-int ball_pick_dir = 17;
-int ball_pick_pwm = 16;
+int ball_pick_dir = 25;
+int ball_pick_pwm = 32;
+
+int curr_pwm = 0;
+bool grab_flag = 0;
 
 void setup() {
   // put your setup code here, to run once:
   // PS4.begin("30:94:35:32:df:3e");
-  // PS4.begin("18:26:49:32:57:0C");
-  PS4.begin("cc:66:0a:c9:41:d1");
+  PS4.begin("18:26:49:32:57:0C");
+  // PS4.begin("cc:66:0a:c9:41:d1");
   Serial.begin(115200);
   
   ledcSetup(0, 5000, 8); //loco1 
@@ -48,8 +51,8 @@ void setup() {
   pinMode(ball_pick_dir, OUTPUT); // Ball Pick Dir
   pinMode(ball_pass_dir, OUTPUT); // Ball Pass Dir
 
-  pinMode(13, INPUT); // Top Limit Switch
-  pinMode(12, INPUT); // Bottom Limit Switch
+  pinMode(35, INPUT); // Top Limit Switch
+  pinMode(34, INPUT); // Bottom Limit Switch
 }
 
 void loop() {
@@ -96,53 +99,79 @@ void loop() {
   //  L1&L2 -> Ball Pick
   //  R1&R2 -> Ball Pass
     
-      if (PS4.Square()){
+      if (PS4.Square()){ //Lagori Grab
           Serial.println("Square");
-          ledcWrite(2, 160);
+          ledcWrite(2, 100);
           digitalWrite(lagori_grab_dir, HIGH);
+          curr_pwm = 160;
+          grab_flag = 1;
       }
-      else if ((PS4.Triangle()) && (!digitalRead(13))){
+      else if ((PS4.Triangle()) && (digitalRead(35))){ // Not kadhlay for testing. LIMIT SWITCH lavla ki parat taaka. 
+        // Lagori Lift
         Serial.println("Triangle");
         ledcWrite(3, 160);
         digitalWrite(lagori_lift_dir, HIGH);
       }
-      else if (PS4.Circle()){
+      else if (PS4.Circle()){ //Lagori Grab
         Serial.println("Circle");
-        ledcWrite(2, 160);
-        digitalWrite(lagori_grab_dir, HIGH);
+        ledcWrite(2, 100);
+        digitalWrite(lagori_grab_dir, LOW);
+        curr_pwm = 160;
+        grab_flag = 1;
       }
-      else if ((PS4.Cross()) && (!digitalRead(12))){
+      else if ((PS4.Cross()) && (digitalRead(34))){ // Not kadhlay for testing. LIMIT SWITCH lavla ki parat taaka.
+        // Lagori Lift down
         Serial.println("Cross");
         ledcWrite(3, 160);
         digitalWrite(lagori_lift_dir, LOW);
       }
-      else if (PS4.L1()){
+      else if (PS4.L1()){ // Ball pass
         Serial.println("L1");
         ledcWrite(4, 160);
         digitalWrite(ball_pass_dir, LOW);
       }
-      else if (PS4.L2()){
+      else if (PS4.L2()){ // Ball Pass
         Serial.println("L2");
         ledcWrite(4, 160);
         digitalWrite(ball_pass_dir, HIGH);
       }
-      else if (PS4.R1()){
+      else if (PS4.R1()){ // ball pick
         Serial.println("R1");
         ledcWrite(5, 160);
         digitalWrite(ball_pick_dir, LOW);
       }
-      else if (PS4.R2()){
+      else if (PS4.R2()){ // Ball Pick
         Serial.println("R2");
         ledcWrite(5, 160);
         digitalWrite(ball_pick_dir, HIGH);
       }
       else{
-        ledcWrite(2, 0);
+        //ledcWrite(2, 0);
         ledcWrite(3, 0);
         ledcWrite(4, 0);
+        grab_flag = 0;
+        Serial.println(grab_flag);
+        Serial.println(curr_pwm);
         ledcWrite(5, 0);
+//        while (curr_pwm>0){
+//          cur_pwm -= 20;
+//          ledcWrite(5, curr_pwm);
+//          delay(500); //Use software interrupt later. Te,porary used de;ay fpr the controlled stopping.
+//        }
       }
     }
+    
+    while ((curr_pwm>0) && (!grab_flag)){
+        curr_pwm -= 50;
+        if (curr_pwm<0){
+          curr_pwm=0;
+        }
+        ledcWrite(2, curr_pwm);
+        delay(100); //Use software interrupt later. Temporary used delay fpr the controlled stopping.
+        Serial.println(curr_pwm);
+      }
+//    curr_pwm=0;
+//    ledcWrite(5, curr_pwm);
     prev_time = millis();
   }
 }
